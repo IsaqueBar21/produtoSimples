e3a9abd9bed8a159204eff679be5c4e227a4ffbd
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-09-19 07:27:08 -0300

    primeiro commit

---
+++ produtoSimples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a, b, PROD;
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        PROD = a * b;
+
+        Console.WriteLine("PROD = " + PROD);
+
+    }
+
+}
